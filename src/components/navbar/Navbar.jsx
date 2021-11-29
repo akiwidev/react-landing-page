94caf02dc8325ae332954e36bb7bb543d86e81b8
cheri kennedy <cheri.k3nn3dy@gmail.com>
2021-11-29 10:47:49 +0900

    added jsx skeleton to navbar component and added assets to project

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,19 @@
 import React from "react";
 import "./navbar.css";
+import { RiMenu3Line, RiCloseLin } from "react-icons/ri";
+
+// Using CSS BEM (Block Element Modifier) syntax
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="gpt3__navbar">
+      <div className="gpt3__navbar-links">
+        <div className="gpt3__navbar-links_logo">
+          <img src={logo} alt="logo" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
