866da8375426f8581eb9e86e265e1301e3f7d936
cheri kennedy <cheri.k3nn3dy@gmail.com>
2021-11-29 15:56:13 +0900

    added html elements to header component

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,8 +1,35 @@
 import React from "react";
 import "./header.css";
+import people from "../../assets/people.png";
+import ai from "../../assets/ai.png";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="gpt3__header section__padding" id="home">
+      <div className="gpt3__header-content">
+        <h1 className="gradient__text">
+          Let's Build Somthing Amazing with GPT-3 OpenAI
+        </h1>
+        <p>
+          Endless revaluation of reason chaos salvation ubermensch christianity
+          morality virtues dead right convictions transvaluation suicide justice
+          endless right war. Truth madness decrepit merciful derive decieve
+          ocean burying truth grandeur hatred decieve mountains contradict.
+        </p>
+        <div className="gpt3__header-content__input">
+          <input type="email" placeholder="Your Email Address" />
+          <button type="button">Get Started</button>
+        </div>
+        <div className="gpt3__header-content__people">
+          <img src={people} alt="people" />
+          <p>1600 people requested access in the last 24 hours</p>
+        </div>
+        <div className="gpt3__header-image">
+          <img src={ai} alt="ai" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
