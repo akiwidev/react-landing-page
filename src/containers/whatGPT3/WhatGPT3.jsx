ac1b0881e4d08a1fb2e4f3eb31974b4364f2b303
cheri kennedy <cheri.k3nn3dy@gmail.com>
2021-12-04 10:07:31 +0900

    added title and text arguments to Feature component in WhatGPT3 component

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -6,7 +6,10 @@ const WhatGPT3 = () => {
   return (
     <div className="gpt3__whatgpt3 section__margin" id="whpt3">
       <div className="gpt3__whatgpt3-feature">
-        <Feature />
+        <Feature
+          title="What is GPT-3"
+          text="Ocean hatred zarathustra decieve eternal-return fearful play justice madness horror. Faithful suicide christianity prejudice madness justice christianity aversion snare insofar value passion sexuality. Insofar salvation good endless holiest ultimate ideal will. Passion against ideal pious christian."
+        />
       </div>
       <div className="gpt3__whatgpt3-heading">
         <h1 className="gradient__text">
@@ -14,9 +17,19 @@ const WhatGPT3 = () => {
         </h1>
         <p>Explore the library</p>
         <div className="gpt3__whatgpt3-container">
-          <Feature />
-          <Feature />
-          <Feature />
+          <Feature
+            title="Chatbots"
+            text="Deceptions zarathustra sea christian virtues victorious depths. Reason philosophy passion strong faith pinnacle dead. War suicide good overcome evil law fearful abstract spirit pinnacle philosophy. Decrepit insofar revaluation virtues sexuality.
+"
+          />
+          <Feature
+            title="Knowledgebase"
+            text="Holiest faith against aversion horror decieve abstract of christian oneself eternal-return pinnacle. Deceptions of against zarathustra decieve free snare overcome. Suicide derive virtues inexpedient mountains depths mountains overcome overcome."
+          />
+          <Feature
+            title="Education"
+            text="God oneself endless mountains joy zarathustra ultimate virtues insofar. Christianity transvaluation horror grandeur decieve enlightenment self against ubermensch. Depths madness disgust fearful overcome depths. Fearful snare horror justice reason strong."
+          />
         </div>
       </div>
     </div>
