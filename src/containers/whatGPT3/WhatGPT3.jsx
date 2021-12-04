e14e72527333b63489aae5bbf0a29f861436145e
cheri kennedy <cheri.k3nn3dy@gmail.com>
2021-12-04 10:46:29 +0900

    added styling to feature component and fixed div problem in whatgpt3 component

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -8,7 +8,8 @@ const WhatGPT3 = () => {
       <div className="gpt3__whatgpt3-feature">
         <Feature
           title="What is GPT-3"
-          text="Ocean hatred zarathustra decieve eternal-return fearful play justice madness horror. Faithful suicide christianity prejudice madness justice christianity aversion snare insofar value passion sexuality. Insofar salvation good endless holiest ultimate ideal will. Passion against ideal pious christian."
+          text="Will decieve ultimate depths horror merciful depths inexpedient. Ideal chaos pious ultimate grandeur insofar merciful ultimate madness self ultimate virtues. Ocean ocean inexpedient eternal-return depths spirit faithful decrepit christian. Intentions enlightenment against transvaluation holiest salvation strong ascetic sea pinnacle. Selfish battle contradict pinnacle reason hatred holiest burying moral ideal. Passion god noble value against battle self philosophy free horror.
+          "
         />
       </div>
       <div className="gpt3__whatgpt3-heading">
@@ -16,21 +17,21 @@ const WhatGPT3 = () => {
           The possibilities are beyond your imagination
         </h1>
         <p>Explore the library</p>
-        <div className="gpt3__whatgpt3-container">
-          <Feature
-            title="Chatbots"
-            text="Deceptions zarathustra sea christian virtues victorious depths. Reason philosophy passion strong faith pinnacle dead. War suicide good overcome evil law fearful abstract spirit pinnacle philosophy. Decrepit insofar revaluation virtues sexuality.
+      </div>
+      <div className="gpt3__whatgpt3-container">
+        <Feature
+          title="Chatbots"
+          text="Deceptions zarathustra sea christian virtues victorious depths. Reason philosophy passion strong faith pinnacle dead. War suicide good overcome evil law fearful abstract spirit pinnacle philosophy. Decrepit insofar revaluation virtues sexuality.
 "
-          />
-          <Feature
-            title="Knowledgebase"
-            text="Holiest faith against aversion horror decieve abstract of christian oneself eternal-return pinnacle. Deceptions of against zarathustra decieve free snare overcome. Suicide derive virtues inexpedient mountains depths mountains overcome overcome."
-          />
-          <Feature
-            title="Education"
-            text="God oneself endless mountains joy zarathustra ultimate virtues insofar. Christianity transvaluation horror grandeur decieve enlightenment self against ubermensch. Depths madness disgust fearful overcome depths. Fearful snare horror justice reason strong."
-          />
-        </div>
+        />
+        <Feature
+          title="Knowledgebase"
+          text="Holiest faith against aversion horror decieve abstract of christian oneself eternal-return pinnacle. Deceptions of against zarathustra decieve free snare overcome. Suicide derive virtues inexpedient mountains depths mountains overcome overcome."
+        />
+        <Feature
+          title="Education"
+          text="God oneself endless mountains joy zarathustra ultimate virtues insofar. Christianity transvaluation horror grandeur decieve enlightenment self against ubermensch. Depths madness disgust fearful overcome depths. Fearful snare horror justice reason strong."
+        />
       </div>
     </div>
   );
